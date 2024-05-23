113218b01669a5b0f83d1c39b7a662128c0906e1
MarAntBQ <github@marantbq.dev>
2024-05-23 13:40:07 -0500

    Testing debug

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,7 @@
 "use strict";
 let mensaje = "Hola Mundo";
 mensaje = "MarAntBQ.dev";
+mensaje = "Bye World";
+console.log(mensaje + ' test in console.log');
+let number = 15;
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 let mensaje: string = "Hola Mundo";
 
 mensaje = "MarAntBQ.dev";
+
+mensaje = "Bye World";
+
+console.log(mensaje + ' test in console.log');
+
+let number: number = 15;
