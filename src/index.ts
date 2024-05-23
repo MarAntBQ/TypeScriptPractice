d71cb315423b1e89de7118b917d5586a24f01077
MarAntBQ <github@marantbq.dev>
2024-05-23 13:50:18 -0500

    Learn about any

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,38 @@ mensaje = "Bye World";
 
 console.log(mensaje + ' test in console.log');
 
-let number: number = 15;
+/**
+* JS Types 
+* number
+ * string
+ * boolean
+ * null
+ * undefined
+ * object
+ * function
+ * 
+ * TS Types
+ * any
+ * unkown
+ * never
+ * arrays
+ * tuplas
+ * Enums
+ * 
+ *  Inferred Types
+ * 
+*/
+// Declaring Type
+let extincionDinosaurio: number = 76_000_000
+let favoriteDinosaur: string = "T-Rex"
+// Not declaring Type -> TS will auto detect and assign
+let extinted = true
+// Any
+let myVariable
+myVariable = "Happy Dog"
+myVariable = 42
+
+
+function happyDog(config: any) {
+  return config
+}
